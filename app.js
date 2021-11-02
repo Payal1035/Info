59cc26f542c43e0231f8c70e674b8bd2dcd11c8e
Payal1035 <payalkumari1035@gmail.com>
2021-11-02 15:16:15 +0530

    commit 6

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger';
+const mobile_menu = document.querySelector('.header .nav .nav-list ul');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => (
+    const hamburger: Element ,('active');
+
+)) 
